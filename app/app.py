fd37d0a24ab83229dd60e433117dba2c46a037ee
Christian Diener <ch.diener@gmail.com>
2019-06-05 15:45:20 -0700

    updates to app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,63 @@ import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
 from start import samples, find_closest
+
+
+close = pd.Series(0, index=samples.index)
+
+
+def beta_figure(close, size=16):
+    s = samples[close == 0]
+    ns = samples[close == 1]
+    return {
+            "data": [
+                go.Scattergl(
+                    name="",
+                    x=s.PC1,
+                    y=s.PC2,
+                    showlegend=False,
+                    text=[
+                        "Bacteroidetes: %.1f%%<br />Firmicutes: %.1f%%" %
+                        (r["Bacteroidetes"] * 100, r["Firmicutes"] * 100)
+                        for _, r in s.iterrows()],
+                    mode="markers",
+                    marker={
+                        "color": s.Bacteroidetes - s.Firmicutes,
+                        "colorscale": "RdBu",
+                        "showscale": True,
+                        "size": (s.Bacteroidetes + s.Firmicutes) * size,
+                        "line": {"width": 1,
+                                 "color": "white"},
+                        "opacity": 0.75
+                    }
+                ),
+                go.Scattergl(
+                    name="",
+                    x=ns.PC1,
+                    y=ns.PC2,
+                    showlegend=False,
+                    text=[
+                        "Bacteroidetes: %.1f%%<br />Firmicutes: %.1f%%" %
+                        (r["Bacteroidetes"] * 100, r["Firmicutes"] * 100)
+                        for _, r in ns.iterrows()],
+                    mode="markers",
+                    marker={
+                        "color": "#F8BBD0",
+                        "showscale": False,
+                        "size": max(size * 1.25, 2),
+                        "line": {"width": size / 3,
+                                 "color": "#C51162"},
+                        "opacity": 1.0
+                    }
+                )
+            ],
+            "layout": go.Layout(
+                title="Bray-Curtis PCoA",
+                hovermode="closest",
+                coloraxis={"cmax": -1, "cmax": 1})
+        }
 
 
 app = dash.Dash(__name__, external_stylesheets=[
@@ -19,58 +75,74 @@ app.layout = html.Div(style={"max-width": "1000px", "margin": 0,
         html.P("Find close participants in the American Gut project."),
         html.P("What is the percentage of Bacteroides and Firmicutes you want"
                " to query?")
-    ]),
+    ], style={"box-shadow": "1px 1px 3px #aaa", "border": "1px solid gray",
+              "border-radius": "5px", "margin": "1em", "padding": "0.25em 1em",
+              "max-width": "90%"}),
     html.Br(),
-
+    html.Div([
     daq.Slider(
         id="bac_slider",
         min=0,
         max=100,
         step=1,
         value=40,
-        size=400,
+        size=360,
         color="red",
         handleLabel={"showCurrentValue": True, "label": "Bacteroidetes"}
     ),
+    html.Br(),
     daq.Slider(
         id="firm_slider",
         min=0,
         max=100,
         step=1,
         value=20,
-        size=400,
+        size=360,
         color="blue",
         handleLabel={"showCurrentValue": True, "label": "Firmicutes"}
-    ),
-
+    )], style={"display": "flex", "justify-content": "space-around",
+               "flex-wrap": "wrap"}),
     dcc.Graph(
         id="phyla_graph",
-        figure={
-            "data": [
-                go.Scatter(
-                    x=samples.PC1,
-                    y=samples.PC2,
-                    text=[
-                        "Bacteroides: %.1f%%<br />Firmicutes: %.1f%%" %
-                        (s["Bacteroidetes"] * 100, s["Firmicutes"] * 100)
-                        for _, s in samples.iterrows()],
-                    mode="markers",
-                    opacity=0.75,
-                    marker={
-                        "color": samples.Bacteroidetes - samples.Firmicutes,
-                        "colorscale": "RdBu",
-                        "showscale": True,
-                        "size": (samples.Bacteroidetes + samples.Firmicutes) * 20,
-                        "line": {"width": 1, "color": "white"}
-                    }
-                )
-            ],
-            "layout": go.Layout()
-        },
-        style={"height": "80vh"}
+        figure=beta_figure(close),
+        style={"height": "70vh", "margin": 0, "padding": 0}
     ),
-    html.P("Principal components plot of the American Gut participants.")
+    html.Div(["point size",
+    dcc.Slider(
+        id="size_slider",
+        min=2,
+        max=30,
+        step=1,
+        value=16,
+        marks={str(i): str(i) for i in ([2] + list(range(5, 31, 5)))}
+    )], style={"margin": "0 4em"})
 ])
+
+
+@app.callback(dash.dependencies.Output("firm_slider", "max"),
+              [dash.dependencies.Input("bac_slider", "value")],
+              [dash.dependencies.State("firm_slider", "value")])
+def update_firm(bac, firm):
+    return 100 - bac
+
+
+@app.callback(dash.dependencies.Output("bac_slider", "max"),
+              [dash.dependencies.Input("firm_slider", "value")],
+              [dash.dependencies.State("bac_slider", "value")])
+def update_bac(firm, bac):
+    return 100 - firm
+
+
+@app.callback(dash.dependencies.Output("phyla_graph", "figure"),
+              [dash.dependencies.Input("firm_slider", "value"),
+               dash.dependencies.Input("bac_slider", "value"),
+               dash.dependencies.Input("size_slider", "value")])
+def update_figure(firm, bac, s):
+    best = find_closest(bac/100, firm/100, samples, n=5)
+    close[:] = 0
+    close[best.index] = 1
+    return beta_figure(close, s)
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- app/start.py
+++ app/start.py
@@ -1,23 +1,21 @@
 """Stuff to run on app startup."""
 
-import logging
 import pickle
 import pandas as pd
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s")
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
 
-
-def find_closest(bacteroides, firmicutes, n=1):
+def find_closest(bacteroidetes, firmicutes, samples, n=1):
     """Find the id of the members closest to the input.
 
     Parameters
     ==========
-    bacteroides : float in [0, 1]
+    bacteroidetes : float in [0, 1]
         The fraction of bacteroides.
     firmicutes : float in [0, 1]
         The fraction of firmicutes.
+    samples : pandas.DataFrame
+        The sample data frame. Must contain column `Bacteroidetes` and
+        `Firmicutes` that contain the percentage of those phyla.
 
     Returns
     =======
@@ -25,19 +23,40 @@ def find_closest(bacteroides, firmicutes, n=1):
         The id of the n closest individuals.
 
     """
-    pass
+    dists = samples.apply(lambda df: (df["Bacteroidetes"] - bacteroidetes)**2 +
+                                     (df["Firmicutes"] - firmicutes)**2,
+                          axis=1)
+    return dists.sort_values()[0:n]
 
 
+# We start by reading our genus level data
 genera = pd.read_csv("../data/american_gut_genus.csv", dtype={"id": str})
+
+# Here we calculate the "library size", the total sum of counts/reads for
+# each sample
 libsize = genera.groupby("id")["count"].sum()
+
+# This is just the metadata
 meta = pd.read_csv("../data/metadata.tsv", dtype={"id": str}, sep="\t")
 
+# Now we want to summarize the data on the phylum level and convert counts
+# to percentages. We start by summarizing on the phylum level
 phyla = genera.groupby(["id", "Phylum"])["count"].sum().reset_index()
+
+# Now we convert the counts to fractions by dividing by the library size
 phyla["relative"] = phyla["count"].values / libsize[phyla.id].values
+
+# We will only keep the fractions for the two phyla we're interested in
 phyla = pd.pivot_table(phyla, index="id", columns="Phylum", values="relative",
                        fill_value=0)[["Bacteroidetes", "Firmicutes"]]
+
+# As a last step we will load the PCoA coordinates generated in
+# `beta_diversity.py`, select 1000 random individuals and merge the
+# coordinates with the phyla abundances
 with open("pcoa.pickle", "rb") as pfile:
     red = pickle.load(pfile)
 
 samples = red.samples.sample(1000)
 samples = pd.merge(samples, phyla, left_index=True, right_index=True)
+
+# The App will now use the samples DataFrame
